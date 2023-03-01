b691a31998d066b7c49c93e1aab6fde3fe47592a
Scoan <Coan.Stephen@gmail.com>
2023-02-28 23:08:32 -0800

    rotate catalog gem

---
+++ Assets/Scripts/MyCamControls/CatalogCameraRotator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace MyCamControls
+{
+    public class CatalogCameraRotator : MonoBehaviour
+    {
+        public float m_rotateRate = 1.0f;
+        
+        public void Update()
+        {
+            transform.Rotate(new Vector3(00f, m_rotateRate * Time.deltaTime, 0.0f));
+        }
+    }
+}
